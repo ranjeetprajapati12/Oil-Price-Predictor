d6619953d53cc11647277fc3dc93c95990fca1bf
Ranjeet Prajapati <ranjeetprajapati12@gmail.com>
2022-11-03 11:57:00 +0530

    This commit is for web application

---
+++ app.py
@@ -0,0 +1,29 @@
+import joblib
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+
+model=joblib.load(open('Oil_Price_Prediction.pkl','rb'))
+scaler=joblib.load(open('opp.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+
+    df2=pd.DataFrame([data],columns=['Date1'])
+    df3=df2['Date1'].astype('datetime64')
+    value=scaler.transform([df3])
+    output=model.predict(value)
+    print(output[0][0])
+    return jsonify(output[0][0])
+
+if __name__=="__main__":
+    app.run(debug=True)
